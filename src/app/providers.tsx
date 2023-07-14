f087ff9657ab4c22d233c94a04e2d1edbeff5f13
brunoBrian <bruno.brian@vemprojuca.com>
2023-07-13 21:58:33 -0300

    import design system

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { globalStyles } from '@/styles/global';
+import { PropsWithChildren } from 'react';
+
+globalStyles()
+
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
